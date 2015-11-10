be53b12bb0479322172d6e0de4e080e9ec5fdd6d
amiliou <athmiliou@gmail.com>
2015-11-10 20:42:54 +0000

    Add test and code for N=1

---
+++ PrimeNumbers/PrimeNumberGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeNumbers
+{
+    public class PrimeNumberGenerator
+    {
+        public static IList<int> Generate(int limit)
+        {
+            var primes = new List<int>();
+
+            if (limit == 1)
+                return primes;
+
+
+            return primes;
+
+        }
+    }
+}
---
+++ Tests/PrimeNumbers.UnitTests/PrimeNumberGeneratorTests.cs
@@ -0,0 +1,19 @@
+﻿
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace PrimeNumbers.UnitTests
+{
+    [TestFixture]
+    public class PrimeNumberGeneratorTests
+    {
+        [Test]
+        public void Should_not_return_any_primes_for_n_equal_to_1()
+        {
+            IList<int> primes = PrimeNumberGenerator.Generate(1);
+
+            Assert.That(primes.Count, Is.EqualTo(0));
+        }
+
+    }
+}
