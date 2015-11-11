ba8a2fb49284de314e6ea0019acd89aedd9fe5cf
amiliou <athmiliou@gmail.com>
2015-11-11 08:21:51 +0000

    add logic to find divisible numbers and unit tests for it

--- PrimeNumbers/DivisorsLocator.cs
+++ PrimeNumbers/DivisorsLocator.cs
@@ -4,8 +4,15 @@ namespace PrimeNumbers
 {
     public class DivisorsLocator : IDivisorsLocator
     {
-        public bool DoesAnyNumberEvenlyDividePrimeNumber(int nextPotentialPrime)
+        public bool DoesAnyNumberEvenlyDividePrimeNumber(int potentialPrime)
         {
+            int maxPotentialDivisor = (int)Math.Sqrt(potentialPrime);
+
+            for (var i = 2; i <= maxPotentialDivisor; i++)
+            {
+                if (potentialPrime % i == 0)
+                    return true;
+            }
             return false;
         }
     }
---
+++ Tests/PrimeNumbers.UnitTests/DivisorsLocatorTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers.UnitTests
+{
+    [TestFixture]
+    public class DivisorsLocatorTests
+    {
+        [Test]
+        [TestCase(9)]
+        [TestCase(27)]
+        [TestCase(447)]
+        public void Should_return_true_if_number_is_evenly_divisible(int nonPrime)
+        {
+            var result = new DivisorsLocator().DoesAnyNumberEvenlyDividePrimeNumber(nonPrime);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        [TestCase(17)]
+        [TestCase(167)]
+        [TestCase(541)]
+        public void Should_return_false_if_number_is_not_evenly_divisible(int prime)
+        {
+            var result = new DivisorsLocator().DoesAnyNumberEvenlyDividePrimeNumber(prime);
+            Assert.That(result, Is.False);
+        }
+    }
+}
