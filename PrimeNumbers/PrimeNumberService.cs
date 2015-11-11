3f950b3bcfd70b8318c8d25f3099ca89aacb7d95
amiliou <athmiliou@gmail.com>
2015-11-11 14:20:12 +0000

    remove unit test for PrimeNumbersService and add Acceptance test instead implement the console output of the grid but no other way to test it other than looking into test output

--- PrimeNumbers/PrimeNumberService.cs
+++ PrimeNumbers/PrimeNumberService.cs
@@ -18,6 +18,63 @@ namespace PrimeNumbers
             var primes = _primeNumberGenerator.Generate(desiredNumberOfPrimes);
             var grid = _gridBuilder.Build(primes);
 
+            ConsoleOutputGrid(grid);
+        }
+
+        private void ConsoleOutputGrid(int[][] grid)
+        {
+            var dimension = grid[0].Length;
+
+            int widestNumber = GetWidestNumber(grid, dimension);
+
+            var numberOfVerticalLines = dimension + 1;
+
+            var widthOfAllColumnsWithWidthOfWidestNumber = dimension * widestNumber;
+
+            var tableWidth = widthOfAllColumnsWithWidthOfWidestNumber + numberOfVerticalLines;
+
+            for (var i = 0; i < dimension; i++)
+            {
+                PrintRow(tableWidth, grid[i]);
+                PrintLine(tableWidth);
+            }
+        }
+
+        private static int GetWidestNumber(int[][] grid, int dimension)
+        {
+            var widestNumber = grid[dimension - 1][dimension - 1].ToString().Length;
+            
+            if (dimension > 10)
+                widestNumber = grid[10][10].ToString().Length;
+
+            return widestNumber;
+        }
+
+        private static void PrintRow(int tableWidth, int[] numbersOfRow)
+        {
+            var columnWidth = (tableWidth - numbersOfRow.Length) / numbersOfRow.Length;
+            var row = "|";
+
+            foreach (var number in numbersOfRow)
+            {
+                row += AlignInColumnCentre(number, columnWidth) + "|";
+            }
+
+            Console.WriteLine(row);
+        }
+
+        private static string AlignInColumnCentre(int number, int width)
+        {
+            if (number == 0)
+                return new string(' ', width);
+
+            var numberText = number.ToString();
+            return numberText.PadRight(width - (width - numberText.Length) / 2).PadLeft(width);
+        }
+
+        private static void PrintLine(int tableWidth)
+        {
+            Console.WriteLine(new string('-', tableWidth));
         }
     }
 }
---
+++ PrimeNumbers/PrimeNumbers.AcceptanceTests/PrimeNumbersServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace PrimeNumbers.AcceptanceTests
+{
+    [TestFixture]
+    public class PrimeNumbersServiceTests
+    {
+        private PrimeNumbersService _primeNumbersService;
+
+        [Test]
+        public void Should_console_output_the_primes()
+        {
+            _primeNumbersService = new PrimeNumbersService(new PrimeNumberGenerator(new DivisorsLocator()), new GridBuilder());
+            
+            _primeNumbersService.ConsoleOutputPrimeNumbersGrid(105);
+
+        }
+    }
+}
