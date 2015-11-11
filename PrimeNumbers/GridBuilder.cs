97aeef738ef087ce307dde06ca70d6e0476f0a89
amiliou <athmiliou@gmail.com>
2015-11-11 12:25:41 +0000

    change array to multidimensional array

--- PrimeNumbers/GridBuilder.cs
+++ PrimeNumbers/GridBuilder.cs
@@ -4,10 +4,9 @@ namespace PrimeNumbers
 {
     public class GridBuilder
     {
-        public static int[,] Build(int[] primes)
+        public static int[][] Build(int[] primes)
         {
-            var dimension = primes.Length + 1;
-            var grid = new int[dimension, dimension];
+            int[][] grid = ConstructGridWithDimension(primes.Length + 1);
 
             FillPrimesInFirstRowAndColumn(primes, grid);
             FillMultiplicationResultsBetweenFirstRowAndColumn(primes, grid);
@@ -15,24 +14,35 @@ namespace PrimeNumbers
             return grid;
         }
 
-        private static void FillMultiplicationResultsBetweenFirstRowAndColumn(int[] primes, int[,] grid)
+        private static int[][] ConstructGridWithDimension(int dimension)
+        {
+            var grid = new int[dimension][];
+
+            for (var i = 0; i < dimension; i++)
+                grid[i] = new int[dimension + 1];
+
+            return grid;
+        }
+
+        private static void FillPrimesInFirstRowAndColumn(int[] primes, int[][] grid)
+        {
+            for (var i = 0; i < primes.Length; i++)
+            {
+                grid[0][i + 1] = primes[i];
+                grid[i + 1][0] = primes[i];
+            }
+        }
+
+        private static void FillMultiplicationResultsBetweenFirstRowAndColumn(int[] primes, int[][] grid)
         {
             for (var i = 1; i <= primes.Length && i < 10; i++)
             {
                 for (var j = 1; j <= primes.Length && j < 10; j++)
                 {
-                    grid[i, j] = grid[0, i] * grid[j, 0];
+                    grid[i][j] = grid[0][i] * grid[j][0];
                 }
             }
         }
 
-        private static void FillPrimesInFirstRowAndColumn(int[] primes, int[,] grid)
-        {
-            for (var i = 0; i < primes.Length; i++)
-            {
-                grid[0, i + 1] = primes[i];
-                grid[i + 1, 0] = primes[i];
-            }
-        }
     }
 }
--- Tests/PrimeNumbers.UnitTests/GridBuilderTests.cs
+++ Tests/PrimeNumbers.UnitTests/GridBuilderTests.cs
@@ -11,34 +11,34 @@ namespace PrimeNumbers.UnitTests
         public void Should_get_a_N_plus_1_x_N_plus_1_grid_with_only_the_primes()
         {
             var primes = new int[4] { 2, 3, 5, 7 };
-            var expectedGrid = new int[5, 5]
+            var expectedGrid = new []
             {
-                {0, 2, 3, 5, 7},
-                {2, 0, 0, 0, 0},
-                {3, 0, 0, 0, 0},
-                {5, 0, 0, 0, 0},
-                {7, 0, 0, 0, 0},
+                new [] {0, 2, 3, 5, 7},
+                new [] {2, 0, 0, 0, 0},
+                new [] {3, 0, 0, 0, 0},
+                new [] {5, 0, 0, 0, 0},
+                new [] {7, 0, 0, 0, 0}
             };
 
             var actualGrid = GridBuilder.Build(primes);
 
-            Assert.That(actualGrid[0,1], Is.EqualTo(2));
-            Assert.That(actualGrid[1,0], Is.EqualTo(2));
-            Assert.That(actualGrid[0,4], Is.EqualTo(7));
-            Assert.That(actualGrid[4,0], Is.EqualTo(7));
+            Assert.That(actualGrid[0][1], Is.EqualTo(2));
+            Assert.That(actualGrid[1][0], Is.EqualTo(2));
+            Assert.That(actualGrid[0][4], Is.EqualTo(7));
+            Assert.That(actualGrid[4][0], Is.EqualTo(7));
         }
 
         [Test]
         public void Should_get_a_N_plus_1_x_N_plus_1_grid_with_primes_and_multiplications()
         {
             var primes = new int[4] { 2, 3, 5, 7 };
-            var expectedGrid = new int[5, 5]
+            var expectedGrid = new int[5][]
             {
-                {0, 2,  3,  5,  7},
-                {2, 4,  6,  10, 14},
-                {3, 6,  9,  15, 21},
-                {5, 10, 15, 25, 35},
-                {7, 14, 21, 35, 49},
+                new [] {0, 2,  3,  5,  7},
+                new [] {2, 4,  6,  10, 14},
+                new [] {3, 6,  9,  15, 21},
+                new [] {5, 10, 15, 25, 35},
+                new [] {7, 14, 21, 35, 49}
             };
 
             var actualGrid = GridBuilder.Build(primes);
