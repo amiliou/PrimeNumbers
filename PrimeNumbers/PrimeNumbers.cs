1eef9c8f5acbec31b221861f684148bcafe345ec
amiliou <athmiliou@gmail.com>
2015-11-11 14:32:33 +0000

    add the main program logic to read input from the user and verifying the input

--- PrimeNumbers/PrimeNumbers.cs
+++ PrimeNumbers/PrimeNumbers.cs
@@ -1,10 +1,29 @@
 ﻿
+using System;
+
 namespace PrimeNumbers
 {
     class PrimeNumbers
     {
         static void Main(string[] args)
         {
+            int desiredNumberOfPrimes;
+            Console.WriteLine("Please provide a whole number > 0 for primes generation: ");
+            if (int.TryParse(Console.ReadLine(), out desiredNumberOfPrimes))
+            {
+                if (desiredNumberOfPrimes > 0)
+                {
+                    var primeNumberService = new PrimeNumbersService(new PrimeNumberGenerator(new DivisorsLocator()), new GridBuilder());
+                    primeNumberService.ConsoleOutputPrimeNumbersGrid(desiredNumberOfPrimes);
+                }
+                else
+                    Console.WriteLine("You didn't provide a number > 0.");
+            }
+            else
+
+                Console.WriteLine("You didn't provide a whole number.");
+
+            Console.ReadKey();
         }
     }
 }
