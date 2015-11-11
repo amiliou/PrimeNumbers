b37366b428fd4690f9d7e2e2653060a604cd5ba6
amiliou <athmiliou@gmail.com>
2015-11-11 09:18:50 +0000

    Add logic to fill grid with the multiplication results between first row and column

--- PrimeNumbers/GridBuilder.cs
+++ PrimeNumbers/GridBuilder.cs
@@ -9,13 +9,30 @@ namespace PrimeNumbers
             var dimension = primes.Length + 1;
             var grid = new int[dimension, dimension];
 
-            for (var i = 0; i < primes.Length; i++)
-            {
-                grid[0, i+1] = primes[i];
-                grid[i+1, 0] = primes[i];
-            }
+            FillPrimesInFirstRowAndColumn(primes, grid);
+            FillMultiplicationResultsBetweenFirstRowAndColumn(primes, grid);
 
             return grid;
+        }
+
+        private static void FillMultiplicationResultsBetweenFirstRowAndColumn(int[] primes, int[,] grid)
+        {
+            for (var i = 1; i <= primes.Length && i < 10; i++)
+            {
+                for (var j = 1; j <= primes.Length && j < 10; j++)
+                {
+                    grid[i, j] = grid[0, i] * grid[j, 0];
+                }
+            }
+        }
+
+        private static void FillPrimesInFirstRowAndColumn(int[] primes, int[,] grid)
+        {
+            for (var i = 0; i < primes.Length; i++)
+            {
+                grid[0, i + 1] = primes[i];
+                grid[i + 1, 0] = primes[i];
+            }
         }
     }
 }
--- Tests/PrimeNumbers.UnitTests/GridBuilderTests.cs
+++ Tests/PrimeNumbers.UnitTests/GridBuilderTests.cs
@@ -8,7 +8,7 @@ namespace PrimeNumbers.UnitTests
     public class GridBuilderTests
     {
         [Test]
-        public void Should_get_a_N_plus_1_x_N_plus_1_grid()
+        public void Should_get_a_N_plus_1_x_N_plus_1_grid_with_only_the_primes()
         {
             var primes = new int[4] { 2, 3, 5, 7 };
             var expectedGrid = new int[5, 5]
@@ -22,7 +22,28 @@ namespace PrimeNumbers.UnitTests
 
             var actualGrid = GridBuilder.Build(primes);
 
-            Assert.That(actualGrid, Is.EquivalentTo(expectedGrid));
+            Assert.That(actualGrid[0,1], Is.EqualTo(2));
+            Assert.That(actualGrid[1,0], Is.EqualTo(2));
+            Assert.That(actualGrid[0,4], Is.EqualTo(7));
+            Assert.That(actualGrid[4,0], Is.EqualTo(7));
+        }
+
+        [Test]
+        public void Should_get_a_N_plus_1_x_N_plus_1_grid_with_primes_and_multiplications()
+        {
+            var primes = new int[4] { 2, 3, 5, 7 };
+            var expectedGrid = new int[5, 5]
+            {
+                {0, 2,  3,  5,  7},
+                {2, 4,  6,  10, 14},
+                {3, 6,  9,  15, 21},
+                {5, 10, 15, 25, 35},
+                {7, 14, 21, 35, 49},
+            };
+
+            var actualGrid = GridBuilder.Build(primes);
+
+            Assert.That(actualGrid, Is.EqualTo(expectedGrid));           
         }
     }
 }
