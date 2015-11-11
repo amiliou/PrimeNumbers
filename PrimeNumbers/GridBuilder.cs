4663e97032eb3d253859d118453b82589fd75c41
amiliou <athmiliou@gmail.com>
2015-11-11 08:57:57 +0000

    add a initial grid builder that only places the primes on the first column and row

---
+++ PrimeNumbers/GridBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    public class GridBuilder
+    {
+        public static int[,] Build(int[] primes)
+        {
+            var dimension = primes.Length + 1;
+            var grid = new int[dimension, dimension];
+
+            for (var i = 0; i < primes.Length; i++)
+            {
+                grid[0, i+1] = primes[i];
+                grid[i+1, 0] = primes[i];
+            }
+
+            return grid;
+        }
+    }
+}
---
+++ Tests/PrimeNumbers.UnitTests/GridBuilderTests.cs
@@ -0,0 +1,28 @@
+﻿
+
+using NUnit.Framework;
+
+namespace PrimeNumbers.UnitTests
+{
+    [TestFixture]
+    public class GridBuilderTests
+    {
+        [Test]
+        public void Should_get_a_N_plus_1_x_N_plus_1_grid()
+        {
+            var primes = new int[4] { 2, 3, 5, 7 };
+            var expectedGrid = new int[5, 5]
+            {
+                {0, 2, 3, 5, 7},
+                {2, 0, 0, 0, 0},
+                {3, 0, 0, 0, 0},
+                {5, 0, 0, 0, 0},
+                {7, 0, 0, 0, 0},
+            };
+
+            var actualGrid = GridBuilder.Build(primes);
+
+            Assert.That(actualGrid, Is.EquivalentTo(expectedGrid));
+        }
+    }
+}
